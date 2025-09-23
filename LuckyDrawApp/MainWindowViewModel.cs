8c2fe9f1ec06d02f2d03085d6ccf225b485933cf
Tri Nguyen <tri.nguyen@sioux.asia>
2025-09-23 21:12:01 +0700

    Clean up warnings and suggestions

--- LuckyDrawApp/MainWindowViewModel.cs
+++ LuckyDrawApp/MainWindowViewModel.cs
@@ -7,174 +7,149 @@ using System.Windows.Media;
 
 namespace LuckyDrawApp
 {
-   public class MainWindowViewModel : INotifyPropertyChanged
-   {
-      private readonly Random _random = new Random();
-      private readonly MediaPlayer _mediaPlayer = new MediaPlayer();
-      private readonly ICommand _startCommand;
-      private readonly List<string> _luckyNumberList = new List<string>();
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private readonly Random _random = new();
 
-      private bool _isSpinning = false;
-      private string _firstDigit = "0";
-      private string _midDigit = "0";
-      private string _lastDigit = "0";
-      private string _luckyNumber = "000";
+        private bool _isSpinning = false;
+        private string _firstDigit = "0";
+        private string _midDigit = "0";
+        private string _lastDigit = "0";
+        private string _luckyNumber = "000";
 
-      public MainWindowViewModel()
-      {
-         _startCommand = new DelegateCommand<int?>(StartSpinning);
-         _luckyNumberList = GetLuckyNumberList(Helper.LUCKY_NUMBER_LIST + Helper.CSV_EXTENSION);
-      }
+        public MainWindowViewModel()
+        {
+            StartCommand = new DelegateCommand<int?>(StartSpinning);
+            LuckyNumberList = GetLuckyNumberList(Helper.LUCKY_NUMBER_LIST + Helper.CSV_EXTENSION);
+        }
 
-      public bool IsSpinning
-      {
-         get => _isSpinning;
-         set
-         {
-            _isSpinning = value;
-            OnPropertyChanged(nameof(IsSpinning));
-         }
-      }
-      public string FirstDigit
-      {
-         get => _firstDigit;
-         set
-         {
-            _firstDigit = value;
-            OnPropertyChanged(nameof(FirstDigit));
-         }
-      }
-      public string MidDigit
-      {
-         get => _midDigit;
-         set
-         {
-            _midDigit = value;
-            OnPropertyChanged(nameof(MidDigit));
-         }
-      }
-      public string LastDigit
-      {
-         get => _lastDigit;
-         set
-         {
-            _lastDigit = value;
-            OnPropertyChanged(nameof(LastDigit));
-         }
-      }
-
-      public MediaPlayer MediaPlayer => _mediaPlayer;
-
-      public string LuckyNumber
-      {
-         get => _luckyNumber;
-         set
-         {
-            _luckyNumber = value;
-            FirstDigit = _luckyNumber.First().ToString();
-            MidDigit = _luckyNumber[1].ToString();
-            LastDigit = _luckyNumber.Last().ToString();
-            OnPropertyChanged(nameof(LuckyNumber));
-         }
-      }
-
-      public List<string> LuckyNumberList => _luckyNumberList;
-
-      public ICommand StartCommand => _startCommand;
-
-      public event PropertyChangedEventHandler? PropertyChanged;
-
-      public async void StartSpinning(int? wantedNumber = null)
-      {
-         if (_luckyNumberList.Count > 0)
-         {
-            if (File.Exists(Helper.SPINNING_EFFECT_PATH))
+        public bool IsSpinning
+        {
+            get => _isSpinning;
+            set
             {
-               _mediaPlayer.Open(new Uri(Helper.SPINNING_EFFECT_PATH, UriKind.RelativeOrAbsolute));
-               _mediaPlayer.Play();
+                _isSpinning = value;
+                OnPropertyChanged(nameof(IsSpinning));
             }
-
-            IsSpinning = true;
-
-            // Adjust the number of spins
-            int numberOfSpins = 10000000;
-            for (int i = 0; i < numberOfSpins; i++)
+        }
+        public string FirstDigit
+        {
+            get => _firstDigit;
+            set
             {
-               if (!IsSpinning) break;
-               LuckyNumber = _luckyNumberList.ElementAt(_random.Next(1, _luckyNumberList.Count));
-               await Task.Delay(100); // Adjust delay for speed
+                _firstDigit = value;
+                OnPropertyChanged(nameof(FirstDigit));
             }
-
-            if (wantedNumber != null)
+        }
+        public string MidDigit
+        {
+            get => _midDigit;
+            set
             {
-               LuckyNumber = wantedNumber?.ToString("D3") ?? string.Empty;
-               if (_luckyNumberList.Contains(LuckyNumber)) _luckyNumberList.Remove(LuckyNumber);
+                _midDigit = value;
+                OnPropertyChanged(nameof(MidDigit));
             }
-
-            if (_mediaPlayer.Source != null) _mediaPlayer.Stop();
-
-            if (File.Exists(Helper.AFTER_SPIN_EFFECT_PATH))
+        }
+        public string LastDigit
+        {
+            get => _lastDigit;
+            set
             {
-               _mediaPlayer.Open(new Uri(Helper.AFTER_SPIN_EFFECT_PATH, UriKind.RelativeOrAbsolute));
-               _mediaPlayer.Play();
+                _lastDigit = value;
+                OnPropertyChanged(nameof(LastDigit));
             }
+        }
 
-            IsSpinning = false;
-         }
-         else
-         {
-            MessageBox.Show("No lucky number data available!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-         }
-      }
+        public MediaPlayer MediaPlayer { get; } = new();
 
-      // Helper method to wait for mouse click asynchronously
-      private static async Task WaitForMouseClickAsync(TaskCompletionSource<bool> clickTaskSource)
-      {
-         void handler(object s, MouseButtonEventArgs e)
-         {
-            clickTaskSource.SetResult(true);
-            Mouse.RemovePreviewMouseDownHandler(Application.Current.MainWindow, handler);
-         }
-
-         Mouse.AddPreviewMouseDownHandler(Application.Current.MainWindow, handler);
-
-         await clickTaskSource.Task;
-      }
-
-      private void OnPropertyChanged(string propertyName)
-      {
-         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-      }
-
-      private static List<string> GetLuckyNumberList(string filePath)
-      {
-         List<string> luckyNumberList = new List<string>();
-         if (File.Exists(filePath))
-         {
-            using (var reader = new StreamReader(filePath))
+        public string LuckyNumber
+        {
+            get => _luckyNumber;
+            set
             {
-               while (!reader.EndOfStream)
-               {
-                  string? line = reader.ReadLine();
-                  if (line != null && line.Length == 3 && IsDigitsOnly(line))
-                  {
-                     luckyNumberList.Add(line);
-                  }
-               }
+                _luckyNumber = value;
+                FirstDigit = _luckyNumber[0].ToString();
+                MidDigit = _luckyNumber[1].ToString();
+                LastDigit = _luckyNumber[^1].ToString();
+                OnPropertyChanged(nameof(LuckyNumber));
             }
-         }
-         return luckyNumberList;
-      }
+        }
 
-      private static bool IsDigitsOnly(string str)
-      {
-         foreach (char c in str)
-         {
-            if (c < '0' || c > '9')
-               return false;
-         }
+        public List<string> LuckyNumberList { get; }
 
-         return true;
-      }
-   }
+        public ICommand StartCommand { get; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public async void StartSpinning(int? wantedNumber = null)
+        {
+            if (LuckyNumberList.Count > 0)
+            {
+                if (File.Exists(Helper.SPINNING_EFFECT_PATH))
+                {
+                    MediaPlayer.Open(new Uri(Helper.SPINNING_EFFECT_PATH, UriKind.RelativeOrAbsolute));
+                    MediaPlayer.Play();
+                }
+
+                IsSpinning = true;
+
+                // Adjust the number of spins
+                const int numberOfSpins = 10000000;
+                for (int i = 0; i < numberOfSpins; i++)
+                {
+                    if (!IsSpinning) break;
+                    LuckyNumber = LuckyNumberList[_random.Next(1, LuckyNumberList.Count)];
+                    await Task.Delay(100); // Adjust delay for speed
+                }
+
+                if (wantedNumber != null)
+                {
+                    LuckyNumber = wantedNumber?.ToString("D3") ?? string.Empty;
+                    if (LuckyNumberList.Contains(LuckyNumber)) LuckyNumberList.Remove(LuckyNumber);
+                }
+
+                if (MediaPlayer.Source != null) MediaPlayer.Stop();
+
+                if (File.Exists(Helper.AFTER_SPIN_EFFECT_PATH))
+                {
+                    MediaPlayer.Open(new Uri(Helper.AFTER_SPIN_EFFECT_PATH, UriKind.RelativeOrAbsolute));
+                    MediaPlayer.Play();
+                }
+
+                IsSpinning = false;
+            }
+            else
+            {
+                MessageBox.Show("No lucky number data available!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private static List<string> GetLuckyNumberList(string filePath)
+        {
+            List<string> luckyNumberList = [];
+            if (File.Exists(filePath))
+            {
+                using var reader = new StreamReader(filePath);
+                while (!reader.EndOfStream)
+                {
+                    string? line = reader.ReadLine();
+                    if (line?.Length == 3 && IsDigitsOnly(line))
+                    {
+                        luckyNumberList.Add(line);
+                    }
+                }
+            }
+            return luckyNumberList;
+        }
+
+        private static bool IsDigitsOnly(string str)
+        {
+            return !str.Any(c => c < '0' || c > '9');
+        }
+    }
 }
